65889be1df01671f0718f43b79428db7a96c05f3
Naveen <104629665+Naveen-SS-03@users.noreply.github.com>
2022-06-11 09:43:10 +0530

    Update Account.java

--- src/main/java/com/capg/entity/Account.java
+++ src/main/java/com/capg/entity/Account.java
@@ -7,59 +7,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
 
-
+@Data
 @Entity
 public class Account {
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private int id;
- private int accountnumber;
- private String accountholdername;
- private String accounttype;
- private BigDecimal accountBalance;
- 
- public Account() {
- }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int accountnumber;
+	private String accountholdername;
+	private String accounttype;
+	private int accountholderage;
+	private String address;
+	private String pancardnumber;
+	private String nationality;
+	private BigDecimal accountBalance;
 
- public int getId() {
-	return id;
-}
+	public Account() {
+	}
 
-public void setId(int id) {
-	this.id = id;
-}
-
-public int getAccountnumber() {
-	return accountnumber;
-}
-
-public void setAccountnumber(int accountnumber) {
-	this.accountnumber = accountnumber;
-}
-
-public String getAccountholdername() {
-	return accountholdername;
-}
-
-public void setAccountholdername(String accountholdername) {
-	this.accountholdername = accountholdername;
-}
-
-public String getAccounttype() {
-	return accounttype;
-}
-
-public void setAccounttype(String accounttype) {
-	this.accounttype = accounttype;
-}
-
-public BigDecimal getAccountBalance() {
-	return accountBalance;
-}
-
-public void setAccountBalance(BigDecimal accountBalance) {
-	this.accountBalance = accountBalance;
-}
- 
 }
